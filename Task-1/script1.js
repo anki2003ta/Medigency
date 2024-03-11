70901baed113ce5d5e423619c6ba8bcdfd8e926f
anki2003ta <nathankita2003@gmail.com>
2024-03-11 15:16:18 +0530

    5th commit done

---
+++ Task-1/script1.js
@@ -0,0 +1,23 @@
+const ratingsE1 = document.querySelectorAll(".rating");
+const sendBtn = document.querySelector(".btn");
+const panel = document.querySelector(".panel-container");
+
+
+ratingsE1.forEach((el) => {
+  el.addEventListener("click",() => {
+    ratingsE1.forEach((innerE1) => {
+      innerE1.classList.remove("active");
+    });
+    el.classList.add("active");
+  });
+});
+sendBtn.addEventListener("click", () => {
+  panel.innerHTML = `<strong>Thank You So Much For Your FeedBack!</strong>
+  <div class="rating rating1">
+    <img src="img13.jpg" class="js1">
+  </div>
+  <p>We'll use your your feedback to inprove our customer support performance</p>`;
+});
+
+
+
